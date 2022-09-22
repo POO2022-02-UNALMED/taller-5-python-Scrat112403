c428025a9d5535d0dd6686c7a531339f1bd625f2
Scrat1124 <111265350+Scrat112403@users.noreply.github.com>
2022-09-22 02:16:50 -0500

    A

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -31,3 +31,35 @@ class Animal:
             return "Mi nombre es "+self._nombre +"tengo una edad de "+ self._edad + "habito en " + self._habitat + "y mi genero es " + self._genero + "la zona en la que me ubico es "+ self._zona + "en el " + self._zoo
         else:
             return "Mi nombre es "+self._nombre +"tengo una edad de "+ self._edad + "habito en " + self._habitat + "y mi genero es " + self._genero
+    
+    def setNombre(self, nombre):
+        self._nombre= nombre
+
+    def getNombre(self):
+        return self._nombre 
+    
+    def setEdad(self, edad):
+        self._edad= edad
+
+    def getEdad(self):
+        return self._edad
+    
+    def setHabitat(self, habitat):
+        self._habitat= habitat
+
+    def getHabitat(self):
+        return self._habitat 
+    
+    def setGenero(self, genero):
+        self._genero= genero
+
+    def getGenero(self):
+        return self._genero
+    
+    def setZona(self, zona):
+        self._zona= zona
+
+    def getZona(self):
+        return self._zona 
+    
+    
