94b1372e43963493ef18ea90b4efe664c56306b1
Scrat1124 <111265350+Scrat112403@users.noreply.github.com>
2022-09-25 00:51:03 -0500

    A

--- zooAnimales/reptil.py
+++ zooAnimales/reptil.py
@@ -1,49 +1,50 @@
 from zooAnimales.animal import Animal
 
 class Reptil(Animal):
-    _listado= []
-    iguanas=0
-    serpientes=0
-    def __init__(self, nombre, edad, habitat, genero, colorEscamas, largoCola):
-        super().__init__(nombre, edad, habitat, genero)
-        self._colorEscamas=colorEscamas
-        self._largoCola=largoCola
-        Reptil._listado.append(self)
-    
-    def movimiento(self):
-        return "reptar"
-    
-    @classmethod
-    def cantidadReptiles(cls):
-        return len(Reptil._listado)
-    
-    @classmethod
-    def crearIguana(cls, nombre, edad, habitat, genero):
-        Reptil.iguanas+=1
-        return Reptil(nombre, edad, "humedal", genero, "verde", 3)
-    
-    @classmethod
-    def crearSerpiente(cls, nombre, edad, habitat, genero):
-        Reptil.serpientes+=1
-        return Reptil(nombre, edad, "jungla", genero, "blanco", 1)
-    
-    def getListado(cls):
-        return Reptil._listado
-    
-    def setListado(cls, listado):
-        Reptil._listado= listado
-    
-    def setColorEscamas(self, color):
-        self._colorEscamas= color
+	_listado = []
+	iguanas = 0
+	serpientes = 0
 
-    def getColorEscamas(self):
-        return self._colorEscamas
-    
-    def setLargoCola(self, largo):
-        self._largoCola= largo
+	def __init__(self,nombre,edad,habitat,genero,colorEscamas,largoCola):
+		super().__init__(nombre,edad,habitat,genero)
+		self._colorEscamas = colorEscamas
+		self._largoCola = largoCola
+		Reptil._listado.append(self)
 
-    def getLargoCola(self):
-        return self._largoCola 
+	def movimiento():
+		return "reptar"
 
-    
+	@classmethod
+	def cantidadReptiles(cls):
+		return len(cls._listado)
+
+	@classmethod
+	def crearIguana(cls,nombre,edad,genero):
+		cls.iguanas+=1
+		return Reptil(nombre,edad,"humedal",genero,"verde",3)
+
+	@classmethod
+	def crearSerpiente(cls,nombre,edad,genero):
+		cls.serpientes+=1
+		return Reptil(nombre,edad,"jungla",genero,"blanco",1)
+
+	def getColorEscamas(self):
+		return self._colorEscamas
+
+	def setColorEscamas(self,colorEscamas):
+		self._colorEscamas = colorEscamas
+
+	
+	def getListado(cls):
+		return cls._listado
+
+	
+	def setListado(cls,listado):
+		cls._listado = listado
+
+	def getLargoCola(self):
+		return self._largoCola
+
+	def setLargoCola(self,largoCola):
+		self._largoCola = largoCola
     
